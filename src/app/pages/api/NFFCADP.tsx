4f2a3ed9c1edb57c36b50e69db0674867d6d2bd3
Andrew M <amess3309@gmail.com>
2024-04-16 15:57:30 -0500

    built in the selectors for different draft types in nffc adp drop down menu, fixed mapping selector, now working as intended

--- src/app/pages/api/NFFCADP.tsx
+++ src/app/pages/api/NFFCADP.tsx
@@ -5,13 +5,19 @@ import { Browser } from "puppeteer";
 
 const url = "https://nfc.shgn.com/adp/football";
 
-const getADPData = async () => {
+const NFFC_Primetime_ADP = async () => {
   const browser: Browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
+  // select draft type drop down menu, then draft type desired
+  await page.select("#draft_type", "option:nth-child(2)");
+  await page.waitForSelector("td:nth-child(2) > a", { timeout: 10000 });
+
   const adpData = await page.evaluate(() => {
-    const playerRows = Array.from(document.querySelectorAll("#adp"));
+    const playerRows = Array.from(
+      document.querySelectorAll("#adp > tbody > tr")
+    );
     // map each row in the table
     const data = playerRows.map((player: any) => ({
       rank: player.querySelector(".rank").innerText,
@@ -27,4 +33,11 @@ const getADPData = async () => {
   await browser.close();
 };
 
-getADPData();
+NFFC_Primetime_ADP();
+
+// primetime option:nth-child(3)
+// rotowire online option:nth-child(4)
+// NFFC BB online option:nth-child(5)
+// classic option:nth-child(11)
+// Guillotine option:nth-child(14)
+// superflex option:nth-child(17)
