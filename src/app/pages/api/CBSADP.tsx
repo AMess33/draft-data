82f325ec98ed31613e4e5c65ba0ffe9847636d54
Andrew M <amess3309@gmail.com>
2024-04-16 14:43:33 -0500

    add adp api for cbs ppr data table

--- src/app/pages/api/CBSADP.tsx
+++ src/app/pages/api/CBSADP.tsx
@@ -2,12 +2,13 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 import { Browser } from "puppeteer";
 
-const url = "https://www.cbssports.com/fantasy/football/draft/averages/";
+const CBSStandard =
+  "https://www.cbssports.com/fantasy/football/draft/averages/";
 
-const getADPData = async () => {
+const CBS_Standard_ADP = async () => {
   const browser: Browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(CBSStandard);
 
   const adpData = await page.evaluate(() => {
     const playerRows = Array.from(
@@ -46,4 +47,46 @@ const getADPData = async () => {
   // });
 };
 
-getADPData();
+// CBS PPR ADP
+// same as CBS Standard ADP selectors, just a different url
+
+const CBSPPR =
+  "https://www.cbssports.com/fantasy/football/draft/averages/ppr/both/h2h/all/";
+
+const CBS_PPR_ADP = async () => {
+  const browser: Browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(CBSPPR);
+
+  const adpData = await page.evaluate(() => {
+    const playerRows = Array.from(
+      document.querySelectorAll(".TableBase-bodyTr")
+    );
+    // map each row in the table
+    const data = playerRows.map((player: any) => ({
+      // player rank data saved as rank
+      rank: player.querySelector(" td:nth-child(1) ").innerText,
+      playerName: player.querySelector(
+        "td:nth-child(2) > span.CellPlayerName--long > span > a"
+      ).innerText,
+      // player position data saved as position
+      position: player
+        .querySelector(
+          "td:nth-child(2) > span.CellPlayerName--long > span > span.CellPlayerName-position"
+        )
+        .innerText.trim(),
+      // player team data saved as team
+      team: player
+        .querySelector(
+          " td:nth-child(2) > span.CellPlayerName--long > span > span.CellPlayerName-team "
+        )
+        .innerText.trim(),
+      // player adp data saved as adp
+      adp: player.querySelector(" td:nth-child(4) ").innerText,
+    }));
+    return data;
+  });
+
+  console.log(adpData);
+  await browser.close();
+};
