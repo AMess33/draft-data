379a5b0272bf385e187967a054293c570a0f9171
Andrew M <amess3309@gmail.com>
2024-04-21 21:14:19 -0500

    espn working

--- src/app/pages/api/ESPNADP.tsx
+++ src/app/pages/api/ESPNADP.tsx
@@ -2,58 +2,179 @@ const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const fs = require("fs");
 import { Browser } from "puppeteer";
-const dayjs = require("dayjs");
 
-const currentDate = dayjs().format("MM-DD-YYYY");
 puppeteer.use(StealthPlugin());
 
 const { executablePath } = require("puppeteer");
 
 const url = "https://fantasy.espn.com/football/livedraftresults";
 
-const ESPN_ADP = async () => {
+(async () => {
   const browser: Browser = await puppeteer.launch({
     headless: false,
+    defaultViewport: false,
     executablePath: executablePath(),
   });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitForSelector(
-    "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__athlete.flex > span > a",
-    { timeout: 10000 }
-  );
 
-  const espnADP = await page.evaluate(() => {
-    const playerRows = Array.from(
-      document.querySelectorAll(".Table__TBODY > tr")
+  let players = new Array();
+  let isBtnDisabled = false;
+  let firstPlayer = "";
+
+  await page.waitForSelector("tbody > tr");
+
+  while (!isBtnDisabled) {
+    console.log("*** RUNNING SCRAPE ***");
+    console.log({ firstPlayer });
+
+    await page.waitForFunction(
+      (p) => {
+        const player = document.querySelector<HTMLDivElement>(
+          "tbody > tr > td:nth-child(2) > div"
+        )?.title;
+
+        if (!p || player !== p) {
+          console.log("*** NEW PAGE, SETTING PLAYER ***");
+          return true;
+        }
+
+        console.log("*** RETURNING FALSE ***");
+        return false;
+      },
+      { polling: 5000, timeout: 60000 },
+      firstPlayer
     );
 
-    const data = playerRows.map((player: any) => ({
-      rank: player.querySelector("td:nth-child(1) > div").innerText,
-      playerName: player.querySelector(
-        "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__athlete.flex > span > a"
-      ).innerText,
-      position: player.querySelector(
-        "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__position.flex > span.playerinfo__playerpos.ttu"
-      ).innerText,
-      team: player.querySelector(
-        "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__position.flex > span.playerinfo__playerteam"
-      ).innerText,
-      adp: player.querySelector("td:nth-child(3) > div").innerText,
-      changeADP: player.querySelector("td:nth-child(4) > div > span").innerText,
-      auctionValue:
-        "$" + player.querySelector("td:nth-child(5) > div").innerText,
-      auctionChange: player.querySelector("td:nth-child(6) > div > span")
-        .innerText,
-    }));
+    const first = await page.evaluate(() => {
+      return document.querySelector<HTMLElement>(
+        "tbody > tr > td:nth-child(2) > div"
+      )?.title;
+    });
 
-    return data;
-  });
-  await page.click(".Pagination__Button--next");
+    firstPlayer = first ?? "";
 
-  console.log(espnADP);
+    console.log("*** GETTING PLAYER ROWS ***");
+
+    const playerRows = await page.$$(".Table__TBODY > tr");
+
+    for (const playerData of playerRows) {
+      let rank: any = "Null";
+      let playerName: any = "Null";
+      let position: any = "Null";
+      let team: any = "Null";
+      let adp: any = "Null";
+      let changeADP: any = "Null";
+      let auctionValue: any = "Null";
+      let auctionChange: any = "Null";
+
+      try {
+        rank = await page.evaluate(
+          (el: any) => el.querySelector("td:nth-child(1) > div").innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        playerName = await page.evaluate(
+          (el: any) =>
+            el.querySelector(
+              "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__athlete.flex > span > a"
+            ).innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        position = await page.evaluate(
+          (el: any) =>
+            el.querySelector(
+              "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__position.flex > span.playerinfo__playerpos.ttu"
+            ).innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        team = await page.evaluate(
+          (el: any) =>
+            el.querySelector(
+              "td:nth-child(2) > div > div > div.jsx-1811044066.player-column_info.flex.flex-column > div > div.jsx-1811044066.player-column__position.flex > span.playerinfo__playerteam"
+            ).innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        adp = await page.evaluate(
+          (el: any) => el.querySelector("td:nth-child(3) > div").innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        changeADP = await page.evaluate(
+          (el: any) =>
+            el.querySelector("td:nth-child(4) > div > span").innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        auctionValue = await page.evaluate(
+          (el: any) => el.querySelector("td:nth-child(5) > div").innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      try {
+        auctionChange = await page.evaluate(
+          (el: any) => el.querySelector("td:nth-child(6) > div").innerText,
+          playerData
+        );
+      } catch (error) {}
+
+      if (rank !== "Null") {
+        players.push({
+          rank,
+          playerName,
+          position,
+          team,
+          adp,
+          changeADP,
+          auctionValue,
+          auctionChange,
+        });
+      }
+    }
+
+    console.log("*** FINDING NEXT BTN ***");
+
+    await page.waitForSelector(".Pagination__Button--next", {
+      visible: true,
+    });
+
+    const is_disabled =
+      (await page.$(".Pagination__Button--next.Button--disabled")) !== null;
+
+    isBtnDisabled = is_disabled;
+    console.log("*** DISABLED:", is_disabled, "***");
+
+    if (!is_disabled) {
+      console.log("*** CLICKING NEXT ***");
+      await page.click(".Pagination__Button--next");
+    }
+  }
+
+  fs.writeFile(
+    "espnADP.json",
+    JSON.stringify({
+      players,
+    }),
+    (err: any) => {
+      if (err) throw err;
+    }
+  );
+
   await browser.close();
-};
-
-ESPN_ADP();
-// this is working, but need to redo the processes to allow for multiple pages
+})();
