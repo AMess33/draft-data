0d35b724c03266d4dee3f353cdf980da82d2edb8
Andrew M <amess3309@gmail.com>
2024-04-22 12:34:55 -0500

    remove console log

--- src/app/pages/api/CBSADP.tsx
+++ src/app/pages/api/CBSADP.tsx
@@ -64,7 +64,6 @@ const CBS_ADP = async (draftTypes: { url: string; lable: string }) => {
     JSON.stringify(adpData),
     (err: any) => {
       if (err) throw err;
-      console.log("The file has been saved!");
     }
   );
 };
