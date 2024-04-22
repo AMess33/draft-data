a1fdba798cce96c109a6e405f1019bd0f4525a18
Andrew M <amess3309@gmail.com>
2024-04-22 12:33:09 -0500

    remove the main function in the page.tsx page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,6 @@
 import Image from "next/image";
 
-const puppeteer = require("puppeteer");
-const fs = require("fs");
-import { Browser } from "puppeteer";
-
-const url = "https://www.cbssports.com/fantasy/football/draft/averages/";
-
-const main = async () => {
-  // launch browser with puppeteer
-  const browser: Browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  // save data on page evaluate in adpData array
-  const adpData = await page.evaluate(() => {
-    const playerRows = Array.from(
-      document.querySelectorAll(".TableBase-bodyTr")
-    );
-    // map each row in the table
-    const data = playerRows.map((player: any) => ({
-      // player rank data saved as rank
-      rank: player.querySelector(" td:nth-child(1) ").innerText,
-      playerName: player.querySelector(
-        "td:nth-child(2) > span.CellPlayerName--long > span > a"
-      ).innerText,
-      // player position data saved as position
-      position: player
-        .querySelector(
-          "td:nth-child(2) > span.CellPlayerName--long > span > span.CellPlayerName-position"
-        )
-        .innerText.trim(),
-      // player team data saved as team
-      team: player
-        .querySelector(
-          " td:nth-child(2) > span.CellPlayerName--long > span > span.CellPlayerName-team "
-        )
-        .innerText.trim(),
-      // player adp data saved as adp
-      adp: player.querySelector(" td:nth-child(4) ").innerText,
-      draftRange: player.querySelector(" td:nth-child(5) ").innerText,
-    }));
-    return data;
-  });
-  // consolog adp data
-  console.log(adpData);
-  await browser.close();
-  // create json file with adpdata
-  fs.writeFile("CBSadp.json", JSON.stringify(adpData), (err: any) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-  });
-};
-
-main();
-
 // boiler plate nextJS homepage html function
-// main() will run on page load/ reload to run again
 
 export default function Home() {
   return (
