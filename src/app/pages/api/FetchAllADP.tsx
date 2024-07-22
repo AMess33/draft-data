a9488bd7ab5d69aef78e39940299349e3f1dba32
Andrew M <amess3309@gmail.com>
2024-07-22 16:27:23 -0500

    add fetch all adp file to execute all adp files on one call, add adp script to json package

---
+++ src/app/pages/api/FetchAllADP.tsx
@@ -0,0 +1,30 @@
+import { exec } from "child_process";
+
+const runFile = (filePath: string) => {
+  exec("npx tsx " + filePath, (error: any, stdout: any, stderr: any) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+};
+
+const files = [
+  "src/app/pages/api/CBSADP.tsx",
+  "src/app/pages/api/DraftKingsADP.tsx",
+  "src/app/pages/api/ESPNADP.tsx",
+  "src/app/pages/api/NFLADP.tsx",
+  "src/app/pages/api/NFFCADP.tsx",
+  "src/app/pages/api/RTSADP.tsx",
+  "src/app/pages/api/SFBADP.tsx",
+  "src/app/pages/api/YahooADP.tsx",
+  "src/app/pages/api/UNDERDOGADP.tsx",
+];
+for (const file of files) {
+  runFile(file);
+}
